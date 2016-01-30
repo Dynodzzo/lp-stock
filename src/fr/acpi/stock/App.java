0847580aa11af1518bb01f97d6cce801738225ed
daoudi el hassan <elha-da@users.noreply.github.com>
2016-01-30 13:34:22 +0100

    Tout n'est pas encore au point! une factory pour le DAL mais encore de pattern observateur.

--- src/fr/acpi/stock/App.java
+++ src/fr/acpi/stock/App.java
@@ -1,11 +1,15 @@
 package fr.acpi.stock;
 
-import fr.acpi.stock.product.controller.ProductController;
-import fr.acpi.stock.product.controller.SalesController;
-import fr.acpi.stock.product.controller.StockController;
-import fr.acpi.stock.product.dal.IProductDAO;
-import fr.acpi.stock.product.dal.ProductDAOFactory;
-import fr.acpi.stock.product.dal.ProductDAOType;
+
+import fr.acpi.stock.controller.catalog.CatalogController;
+import fr.acpi.stock.controller.product.ProductController;
+import fr.acpi.stock.controller.product.SalesController;
+import fr.acpi.stock.controller.product.StockController;
+import fr.acpi.stock.dal.DAOAbstractFactory;
+import fr.acpi.stock.dal.DAOType;
+import fr.acpi.stock.dal.catalog.ICatalogDAO;
+import fr.acpi.stock.dal.product.IProductDAO;
+import fr.acpi.stock.view.HomeWindow;
 import fr.acpi.stock.view.MainWindow;
 
 import javax.swing.*;
@@ -13,11 +17,18 @@ import javax.swing.*;
 public class App {
 
     public static void main(String[] args) {
-		IProductDAO productDAO = ProductDAOFactory.get(ProductDAOType.XML);
-		AppData.Catalog.addProducts(productDAO.getAll());
-		ProductController productCtrl = new ProductController(AppData.Catalog, productDAO);
-		SalesController salesCtrl = new SalesController(AppData.Catalog, productDAO);
-		StockController stockCtrl = new StockController(AppData.Catalog, productDAO);
+		IProductDAO productDAO = DAOAbstractFactory.getInstance(DAOType.XML).Product();
+		ICatalogDAO catalogDAO = DAOAbstractFactory.getInstance(DAOType.XML).Catalog();
+		
+		AppData.catalog.addProducts(productDAO.getAll());
+		AppData.catalogs.addCatalogs(catalogDAO.getAll());
+		
+		ProductController productCtrl = new ProductController(AppData.catalog, productDAO);
+		SalesController salesCtrl = new SalesController(AppData.catalog, productDAO);
+		StockController stockCtrl = new StockController(AppData.catalog, productDAO);
+		CatalogController catalogCtrl = new CatalogController(AppData.catalogs, catalogDAO);
+		
+		JFrame homeWindow = new HomeWindow(catalogCtrl);
 		JFrame mainWindow = new MainWindow(productCtrl, salesCtrl, stockCtrl);
     }
 }
