d2d9f8266d509266414ff550305fddecb74179bb
Dynodzzo <dynodzoo@gmail.com>
2016-02-01 23:54:36 +0100

    Added some javadoc

--- src/fr/acpi/stock/catalog/Catalog.java
+++ src/fr/acpi/stock/catalog/Catalog.java
@@ -12,16 +12,29 @@ public class Catalog implements ICatalog {
 	protected List<IProduct> _products = new ArrayList<>();
 	private String _name;
 
+	/**
+	 * Creates a new catalog with the given name.
+	 * @param name The name of the catalog to create.
+	 */
 	public Catalog(String name) {
 		this._products = new ArrayList<IProduct>();
 		this._name = name;
 	}
-	
+
+	/**
+	 * Returns the name of the catalog.
+	 * @return The name of the catalog.
+	 */
 	@Override
 	public String name() {
 		return this._name;
 	}
 
+	/**
+	 * Adds a product to the catalog.
+	 * @param product The product to add to the catalog.
+	 * @return True if the product has been added, false otherwise.
+	 */
 	@Override
 	public boolean addProduct(IProduct product) {
 		boolean added = false;
@@ -34,6 +47,13 @@ public class Catalog implements ICatalog {
 		return added;
 	}
 
+	/**
+	 * Adds a product to the catalog.
+	 * @param name The name of the product to add to the catalog.
+	 * @param unitPriceET The price excluding taxes of the product to add to the catalog.
+	 * @param amount The amount of pieces of the product to add to the catalog.
+	 * @return True if the product has ben added, false otherwise.
+	 */
 	@Override
 	public boolean addProduct(String name, double unitPriceET, int amount) {
 		boolean added = false;
@@ -47,6 +67,11 @@ public class Catalog implements ICatalog {
 		return added;
 	}
 
+	/**
+	 * Adds multiple products to the catalog.
+	 * @param products A list of products.
+	 * @return The number of products added to the catalog.
+	 */
 	@Override
 	public int addProducts(List<IProduct> products) {
 		int addedProducts = 0;
@@ -62,6 +87,11 @@ public class Catalog implements ICatalog {
 		return addedProducts;
 	}
 
+	/**
+	 * Deletes a product from the catalog.
+	 * @param name The name of the product to delete.
+	 * @return True if the product was deleted, false otherwise.
+	 */
 	@Override
 	public boolean deleteProduct(String name) {
 		boolean removed = false;
@@ -78,6 +108,12 @@ public class Catalog implements ICatalog {
 		return removed;
 	}
 
+	/**
+	 * Buys a product.
+	 * @param productName The name of the product to buy.
+	 * @param amountBought The amount of pieces of the product to buy.
+	 * @return True if the product was bought, false otherwise.
+	 */
 	@Override
 	public boolean buyStock(String productName, int amountBought) {
 		boolean bought = false;
@@ -89,6 +125,12 @@ public class Catalog implements ICatalog {
 		return bought;
 	}
 
+	/**
+	 * Sells a product.
+	 * @param productName The name of the product to sell.
+	 * @param amountSold The amount of pieces of the product to sell.
+	 * @return True if the product was sold, false otherwise.
+	 */
 	@Override
 	public boolean sellStock(String productName, int amountSold) {
 		boolean sold = false;
@@ -100,6 +142,10 @@ public class Catalog implements ICatalog {
 		return sold;
 	}
 
+	/**
+	 * Returns an array containing the names of the products of the catalog in lexicographic order.
+	 * @return An array containing the names of the products of the catalog in lexicographic order.
+	 */
 	@Override
 	public String[] productsNames() {
 		String[] productNames = new String[this._products.size()];
@@ -113,6 +159,10 @@ public class Catalog implements ICatalog {
 		return productNames;
 	}
 
+	/**
+	 * Returns the value of the stock including taxes.
+	 * @return The value of the stock including taxes.
+	 */
 	@Override
 	public double totalAmountIT() {
 		double totalAmount = 0;
@@ -124,16 +174,28 @@ public class Catalog implements ICatalog {
 		return (double)Math.round(totalAmount * 100) / 100;
 	}
 
+	/**
+	 * Returns the product with the specified name.
+	 * @param name The name of the product.
+	 * @return The product with the specified name.
+	 */
 	@Override
 	public IProduct product(String name) {
 		return this._products.get(this.indexOf(name));
 	}
 
+	/**
+	 * Removes all the products from the catalog.
+	 */
 	@Override
 	public void clear() {
 		this._products.clear();
 	}
 
+	/**
+	 * Returns a string representation of the catalog.
+	 * @return A string representation of the catalog.
+	 */
 	@Override
 	public String toString() {
 		DecimalFormat twoDigitsFormat = new DecimalFormat("0.00 €");
@@ -157,6 +219,11 @@ public class Catalog implements ICatalog {
 		return str.toString();
 	}
 
+	/**
+	 * Checks whether a product with a given name exists within the catalog.
+	 * @param name The name of the product to check.
+	 * @return True if the product exists within the catalog, false otherwise.
+	 */
 	protected boolean exists(String name) {
 		boolean exists = false;
 		int productIndex = 0;
@@ -172,6 +239,11 @@ public class Catalog implements ICatalog {
 		return exists;
 	}
 
+	/**
+	 * Checks whether a product is valid.
+	 * @param product The product to check.
+	 * @return True if the product is valid, false otherwise.
+	 */
 	protected boolean valid(IProduct product) {
 		boolean valid = false;
 		boolean notNull = product != null;
@@ -186,6 +258,11 @@ public class Catalog implements ICatalog {
 		return valid;
 	}
 
+	/**
+	 * Returns the index of a product in the catalog's list.
+	 * @param name The name of the product to find.
+	 * @return The index of the product to find or -1 if the product is not found.
+	 */
 	protected int indexOf(String name) {
 		boolean productFound = false;
 		int productIndex = 0;
